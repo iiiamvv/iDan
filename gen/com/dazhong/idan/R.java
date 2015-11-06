6bba7ebc0b3db8a08d49411072841be563a58f8f
iiiamvv <396077415@qq.com>
2015-11-06 13:03:04 +0800

    add layout

--- gen/com/dazhong/idan/R.java
+++ gen/com/dazhong/idan/R.java
@@ -25,14 +25,51 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080000;
-        public static final int textView3=0x7f080002;
+        public static final int action_settings=0x7f080022;
+        public static final int button1=0x7f08000c;
+        public static final int imageView1=0x7f080001;
+        public static final int imageView17=0x7f080017;
+        public static final int imageView2=0x7f080002;
+        public static final int imageView3=0x7f080003;
+        public static final int imageView4=0x7f08000a;
+        public static final int imageView5=0x7f08001d;
+        public static final int layout1=0x7f080018;
+        public static final int layout2=0x7f080019;
+        public static final int layout3=0x7f08001a;
+        public static final int layout4=0x7f08001b;
+        public static final int linearLayout1=0x7f080021;
+        public static final int listView1=0x7f08000b;
+        public static final int login=0x7f08000d;
+        public static final int textView1=0x7f080000;
+        public static final int textView10=0x7f080010;
+        public static final int textView11=0x7f080011;
+        public static final int textView12=0x7f080012;
+        public static final int textView13=0x7f080013;
+        public static final int textView14=0x7f080014;
+        public static final int textView15=0x7f080015;
+        public static final int textView16=0x7f080016;
+        public static final int textView17=0x7f08001c;
+        public static final int textView2=0x7f080005;
+        public static final int textView20=0x7f08001e;
+        public static final int textView21=0x7f08001f;
+        public static final int textView22=0x7f080020;
+        public static final int textView3=0x7f080004;
+        public static final int textView4=0x7f080006;
+        public static final int textView5=0x7f080007;
+        public static final int textView6=0x7f080008;
+        public static final int textView7=0x7f080009;
+        public static final int textView8=0x7f08000e;
+        public static final int textView9=0x7f08000f;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int renwudan=0x7f030001;
+        public static final int add_pay=0x7f030001;
+        public static final int business_list=0x7f030002;
+        public static final int login=0x7f030003;
+        public static final int order_detail=0x7f030004;
+        public static final int order_detail_end=0x7f030005;
+        public static final int order_item=0x7f030006;
+        public static final int renwudan=0x7f030007;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -40,6 +77,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int headline=0x7f050003;
         public static final int hello_world=0x7f050001;
     }
     public static final class style {
